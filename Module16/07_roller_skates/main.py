e51375ae8d4d586ba4d4be5dcaddcd25ef80fecd
Anastasia <lion-n-t@mail.ru>
2021-09-10 22:32:43 +0300

    Переделано 7 задание

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,3 +1,14 @@
+def sort(listN):
+    size = len(listN)
+    for i in range(size - 1):
+        minIndex = i;
+        for j in range(i + 1, size):
+            if listN[j] < listN[minIndex]:
+                minIndex = j
+        tmp = listN[i]
+        listN[i] = listN[minIndex]
+        listN[minIndex] = tmp
+    return listN
 listSkates = []
 listPeople = []
 for i in range(int(input('Кол-во коньков: '))):
@@ -7,16 +18,11 @@ for i in range(int(input('Кол-во людей: '))):
     print('Размер ноги', i+1,'человека:', end=' ')
     listPeople.append(int(input()))
 n = 0
+listSkates = sort(listSkates)
 for j in range(len(listPeople)):
     for i in range(len(listSkates)):
-        if listSkates[i] == listPeople[j]:
+        if listSkates[i] >= listPeople[j]:
             n += 1
             listSkates.remove(listSkates[i])
             break
-    else:
-        for i in range(len(listSkates)):
-            if listSkates[i] > listPeople[j]:
-                n += 1
-                listSkates.remove(listSkates[i])
-                break
 print('Наибольшее кол-во людей, которые могут взять ролики:', n)
