92123f46ed16bf41e7ce1f057d07404a65475267
Anastasia <lion-n-t@mail.ru>
2021-09-16 11:51:50 +0300

    Исправлено 4е задание

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -3,5 +3,5 @@ newS = s[0].upper()
 for i in range(1, len(s)):
     if s[i-1] == ' ' and s[i] != ' ':
         newS += s[i].upper()
-    else: newS += s[i]
+    else: newS += s[i].lower()
 print('Результат:', newS)
