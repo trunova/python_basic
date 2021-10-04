84684130338f5cc670a47af29dc5bf8f56248a95
Anastasia <lion-n-t@mail.ru>
2021-10-04 18:35:39 +0300

    Переделан 2тномер

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,8 +1,18 @@
-# TODO здесь писать код
 def is_prime(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    else: return True
+    if n != 0 and n != 1:
+        for i in range(2, n):
+            if n % i == 0 :
+                return False
+        else: return True
+    else: return False
+
+def listPrimeN(obj):
+    return [value for ind, value in enumerate(obj) if is_prime(ind)]
 
 
+tpl = ('q', 'w', 'e', 'y', 'i', 'j', 'h')
+str = 'qweyijh'
+lst = ['q', 'w', 'e', 'y', 'i', 'j', 'h']
+print(listPrimeN(tpl))
+print(listPrimeN(str))
+print(listPrimeN(lst))
