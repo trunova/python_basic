2618c544fb37df98b7aae8e009c9f6649268caec
Anastasia <lion-n-t@mail.ru>
2021-11-01 22:58:51 +0300

    Домашняя работа № 22 переделан 5 номер

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -2,7 +2,7 @@ import os
 s = input('Введите строку: ')
 p = input('Куда хотите сохранить документ? '
                            'Введите последовательность папок (через пробел):\n').split(' ')
-name = input('\nВведите имя файла:') + '.txt'
+name = input('\nВведите имя файла:')
 pathName = os.path.join(*p, name)
 print(pathName)
 f = open(name, 'r+')
