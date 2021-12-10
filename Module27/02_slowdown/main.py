e895c32848435259fa6626020998e136927815da
trunova <lion-n-t@mail.ru>
2021-12-10 15:34:35 +0300

    Домашняя работа № 27

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import time
+from typing import Callable, Any
+def deceleration(func: Callable) -> Any:
+    def wrapped_func(*args, **kwargs):
+        print('Подождём несколько секунд перед выполнением декорируемой функции.')
+        time.sleep(3)
+        result = func(*args, **kwargs)
+        return result
+    return wrapped_func
+
+@deceleration
+def test():
+    print('<Тут что-то происходит...>')
+
+test()
